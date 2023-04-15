204efe4ce65cc074d3fc1f96f600a0f67e0850c0
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-04-16 00:29:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,40 +2,37 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 #include<string>
-
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  // реализация шаблона очереди с приоритетом на кольцевом буфере 
  private:
     T* arr;
     int first;
     int last;
     int count;
-    
+ 
  public:
     TPQueue() : arr(new T[size]), first(0), last(0), count(0) {}
     ~TPQueue() { delete[] arr; }
     void push(const T& value) {
-        if (count == size)
-            throw std::string("full");
-        else {
-            int position;
-            int i = first;
-            int j = last;
-            while (i < last) {
-                if (arr[i].prior < value.prior) {
-                    position = i;
-                    break;
-                }
-                ++i;
+        if (count == size) throw std::string("full");
+        ++count;
+        int position = last;
+        int i = first;
+        int j = last;
+        while (i < last) {
+            if (arr[i].prior < value.prior) {
+                position = i;
+                break;
             }
-            while (j < position) {
-                arr[i % size] = arr[(i - 1) % size];
-            }
-            arr[(position % size)] = value;
-            ++count;
-            ++last;
+            ++i;
         }
+        while (j < position) {
+            arr[i % size] = arr[(i - 1) % size];
+        }
+        arr[(position % size)] = value;
+        ++count;
+        ++last;
     }
     T& pop() {
         if (0 == count)
@@ -46,7 +43,6 @@ class TPQueue {
         }
     }
 };
-
 struct SYM {
   char ch;
   int prior;
