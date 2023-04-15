4c6abf51859650d1c09a407c5e8cc58cc4d935e9
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-04-16 00:48:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,17 +1,16 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include<string>
+
 template<typename T, int size>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере 
-private:
+class TPQueue { 
+ private:
     T* arr;
     int first;
     int last;
     int count;
 
-public:
+ public:
     TPQueue() : arr(new T[size]), first(0), last(0), count(0) {}
     ~TPQueue() { delete[] arr; }
     void push(const T& value) {
@@ -21,7 +20,8 @@ public:
         int i = first;
         int j = last;
         while (i < last) {
-            if (arr[i].prior < value.prior) { //чекаем позицию согласно приоритету
+            //чекаем позицию согласно приоритету
+            if (arr[i].prior < value.prior) { 
                 position = i;
                 break;
             }
@@ -35,9 +35,9 @@ public:
         last++;
     }
     T& pop() {
-        if (0 == count)
+        if (0 == count) {
             throw std::string("empty");
-        else {
+        } else {
             --count;
             return arr[first++ % size];
         }
